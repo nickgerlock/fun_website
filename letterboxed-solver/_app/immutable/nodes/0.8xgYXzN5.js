0f407596a144f4a7e7161825dc2f02fb9c213500
Nick Gerlock <nickg63@gmail.com>
2024-01-28 21:58:55 -0800

    Remove old letterboxed solver.

--- letterboxed-solver/_app/immutable/nodes/0.8xgYXzN5.js
+++
@@ -1,1 +0,0 @@
-import{s as t,c as n,a as s,b as l}from"../chunks/disclose-version.m045lBee.js";import{p as c,a as u}from"../chunks/runtime.mDm6uTPK.js";const f=!0,i=Object.freeze(Object.defineProperty({__proto__:null,prerender:f},Symbol.toStringTag,{value:"Module"}));function _(e,o){u(o,!1);var a=l(e),r=s(a);t(r,o.children,{},null),n(e,a),c()}export{_ as component,i as universal};
